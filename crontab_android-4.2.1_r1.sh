ab2fec7fa18e2de663429015bb83da5408428e6c
Nfer zhuang <nfer.zhuang@gmail.com>
2015-02-12 22:04:13 +0800

    send mail after update and compile code

--- crontab_android-4.2.1_r1.sh
+++ crontab_android-4.2.1_r1.sh
@@ -4,6 +4,12 @@ ANDROID_4_2=~/android-4.2.1_r1
 CRONTAB_LOG_FOLDER=~/bak/crontablog
 NONEED_TO_REVERT=build/core/main.mk
 
+# mail related variables
+MAIL_FOLDER=~/Mail/
+CONSIGNEE="nfer.zhuang@gmail.com"
+OBJECT="Crontab_build_log"
+SIGNATURE="\nNfer Zhuang\nBest Regards\n"
+
 cd $ANDROID_4_2
 
 HEAD_VER=`svn log -r HEAD | awk '{if(NR==2)print $1}' | cut -b 2-`
@@ -19,4 +25,18 @@ else
     svn st -q | grep ^M | grep -v $NONEED_TO_REVERT | awk '{print $2}' | xargs -i svn revert {} >> $BUILD_LOG
     source build/envsetup.sh >> $BUILD_LOG
     make >> $BUILD_LOG 2>& 1
+
+    # compress BUILD_LOG and send it by mail
+    cd $MAIL_FOLDER
+    cp $BUILD_LOG ./
+    BUILD_LOG_TGZ=`ls *.log | sed 's/log$/tgz/'`
+    tar czf $BUILD_LOG_TGZ *.log
+    MAIL_CONTENT_FILE=`ls *.log | sed 's/log$/mail/'`
+    echo "-----------------comile $HEAD_VER result:-----------------\n" > $MAIL_CONTENT_FILE
+    tail $BUILD_LOG >> $MAIL_CONTENT_FILE
+    echo $SIGNATURE >> $MAIL_CONTENT_FILE
+    mutt -s $OBJECT $CONSIGNEE -a $BUILD_LOG_TGZ < $MAIL_CONTENT_FILE
+
+    # rm unused .tgz and .log file
+    rm *.tgz *.log
 fi
